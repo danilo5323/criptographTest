a3f9be9cba6531d70f2320f554837d777dbd07bf
Danilo Brandao <dan.danilo54@gmail.com>
2021-04-29 23:40:39 -0300

    project start

---
+++ src/main/java/com/example/demo/CriptographTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptographTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptographTestApplication.class, args);
+	}
+
+}
